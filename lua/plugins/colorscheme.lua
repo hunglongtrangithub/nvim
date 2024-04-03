ba7f9a2d48498a806bae5d0315ebec08fc9ef4ee
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-04-03 11:06:59 -0400

    add new colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 local colorscheme = require("lazyvim.plugins.colorscheme")
 return {
   -- add colorscheme
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = "transparent",
-  --       floats = "transparent",
-  --     },
-  --   },
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = true,
@@ -21,49 +21,60 @@ return {
       transparent = true,
     },
   },
-  -- {
-  --   "oxfist/night-owl.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent_background = true, -- apparently this is not working
-  --   },
-  -- },
-  -- {
-  --   "catppuccin",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     flavour = "mocha",
-  --     transparent_background = true,
-  --   },
-  -- },
-  -- {
-  --   "marko-cerovac/material.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     disable = {
-  --       background = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "bluz71/vim-moonfly-colors",
-  --   name = "moonfly",
-  --   lazy = false,
-  --   priority = 1000,
-  -- },
-  -- {
-  --   "rebelot/kanagawa.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --   },
-  -- },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent_background = true, -- apparently this is not working
+    },
+  },
+  {
+    "catppuccin",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+  {
+    "marko-cerovac/material.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      disable = {
+        background = true,
+      },
+    },
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
   {
     "shaunsingh/moonlight.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "thedenisnikulin/vim-cyberpunk",
     lazy = true,
     priority = 1000,
   },
@@ -78,7 +89,9 @@ return {
       -- colorscheme = "catppuccin",
       -- colorscheme = "material",
       -- colorscheme = "moonfly",
-      colorscheme = "moonlight",
+      -- colorscheme = "moonlight",
+      -- colorscheme = "nightfly",
+      colorscheme = "cyberpunk",
     },
   },
 }
