6451c6593a6e052e6cbfb5201e0143bf9f9e8d3e
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-04-15 21:03:33 -0400

    add more languages to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "bash",
         "python",
         "rust",
+        "c",
         "cpp",
+        "lua",
+        "html",
+        "css",
+        "tsx",
+        "javascript",
+        "json",
+        "astro",
       },
     },
   },
