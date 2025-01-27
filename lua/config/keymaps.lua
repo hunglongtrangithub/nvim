e02be727dd85441486edf2f83649a9689bc9df5d
hunglongtrangithub <hunglongtran2004@gmail.com>
2025-01-26 21:09:37 -0500

    fix keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>t", "<cmd>Themery<cr>")
-vim.keymap.set({ "n", "t" }, "<C-/", function()
+vim.keymap.set({ "n", "t" }, "<C-\\>", function()
   require("snacks").terminal.toggle(nil, { interactive = true, win = { position = "float" } })
 end, { noremap = true, silent = true, desc = "Open New Terminal" })
