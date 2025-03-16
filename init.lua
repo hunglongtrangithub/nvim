7704e549ad0b6df83a3ad25c6e5f1f2624c1ba50
Hung Tran <hunglongtran2004@gmail.com>
2025-03-16 19:01:25 -0400

    add comment for cuda

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cuda",
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
+
 require("config.lazy")
