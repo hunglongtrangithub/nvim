ccaa4cc72ebe710c77bd23648d10b0bbec83b566
hunglongtrangithub <hunglongtran2004@gmail.com>
2025-05-10 11:17:08 -0400

    add mason workaround

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "cuda",
-  callback = function()
-    vim.bo.commentstring = "// %s"
-  end,
-})
-
 require("config.lazy")
--- lua/plugins/augmentcode.lua
+++ lua/plugins/augmentcode.lua
@@ -5,5 +5,6 @@ return {
     { "<leader>ac", "<cmd>Augment chat<cr>", mode = { "n", "v" }, desc = "Chat" },
     { "<leader>an", "<cmd>Augment chat-new<cr>", desc = "New Chat" },
     { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Toggle Chat" },
+    { "<c-]>", "<cmd>call augment#Accept()<cr>", mode = { "i" }, { noremap = true, silent = true } },
   },
 }
