1e5e9a2836d565095c106a2900935e92a55c4899
Hung Tran <longhung@amazon.com>
2025-05-26 14:05:39 -0700

    modify regex pattern for highlighting & searching in todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    highlight = {
+      -- custom pattern that accepts "KEYWORD(name):" as well
+      pattern = [[.*<(KEYWORDS)\s*\(?.*\)?:]], -- ripgrep pattern
+      -- default pattern
+      -- pattern = [[.*<(KEYWORDS)\s*:]], -- ripgrep pattern
+    },
+    search = {
+      -- custom pattern that accepts "KEYWORD(name):" as well
+      pattern = [[\b(KEYWORDS)\s*\(?.*\)?:]], -- ripgrep pattern
+      -- default pattern
+      -- pattern = [[.*<(KEYWORDS)\s*:]], -- ripgrep pattern
+    },
+  },
+}
