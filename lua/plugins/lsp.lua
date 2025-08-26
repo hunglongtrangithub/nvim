6dc0e53c88c139f58350fac0eb5dd6638d2c607a
hunglongtrangithub <hunglongtran2004@gmail.com>
2025-08-26 10:39:52 -0400

    add typstyle to format Typst file on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,16 @@ return {
           -- avoid using clangd for protobuf files
           filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" },
         },
+        tinymist = {
+          single_file_support = true, -- Fixes LSP attachment in non-Git directories
+          settings = {
+            formatterMode = "typstyle",
+            exportPdf = "onSave",
+          },
+          lint = {
+            enabled = true,
+          },
+        },
       },
     },
   },
