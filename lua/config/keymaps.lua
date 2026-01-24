0854b0a1283937f099cc22d5ef7ca21a4cf1303e
Hung Tran <hunglongtran2004@gmail.com>
2026-01-23 20:43:13 -0500

    make cursor centered in the screen when searching or scrolling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open Themery to change themes
 vim.keymap.set("n", "<leader>t", "<cmd>Themery<cr>")
+
+-- Toggle terminal
 vim.keymap.set({ "n", "t" }, "<C-\\>", function()
   require("snacks").terminal.toggle(nil, { interactive = true, win = { position = "float" } })
 end, { noremap = true, silent = true, desc = "Open New Terminal" })
+
+-- Keep cursor centered when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+-- Keep cursor centered when searching
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
