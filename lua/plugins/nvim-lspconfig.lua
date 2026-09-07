18b587186fa71bd11fbf15342452b978ee868330
Hung Tran <hunglongtran2004@gmail.com>
2026-09-06 20:47:20 -0700

    check for local zig and zls executables for Zig LSP

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -21,6 +21,29 @@ return {
           },
         },
         zls = {
+          -- check for local zls executable
+          cmd = (function()
+            local cwd = vim.fn.getcwd()
+            local local_zls = cwd .. "/zls/zls"
+            if vim.uv.fs_stat(local_zls) then
+              return { local_zls }
+            end
+            return { "zls" } -- fallback to global
+          end)(),
+          -- check for local zig executable
+          before_init = function(_, config)
+            local root_dir = config.root_dir
+            if not root_dir then
+              return
+            end
+            local local_zig = root_dir .. "/zig/zig"
+            if vim.uv.fs_stat(local_zig) then
+              config.settings = config.settings or {}
+              config.settings.zls = config.settings.zls or {}
+              ---@diagnostic disable-next-line: inject-field
+              config.settings.zls.zig_exe_path = local_zig
+            end
+          end,
           settings = {
             zls = {
               enable_build_on_save = true,
